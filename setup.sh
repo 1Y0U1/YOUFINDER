6053ae8d2aaf5cb465caff5a03711ded515c88f2
Y0U <rubalsharma507@gmail.com>
2025-07-05 12:30:29 +0530

    Update setup.sh

--- setup.sh
+++ setup.sh
@@ -11,15 +11,13 @@ sudo gem install colorize
 echo ""
 sudo gem install optparse
 echo ""
-sudo gem install net/http
+sudo gem install net-http
 echo ""
-sudo gem install socket
-echo ""
-
 sudo chmod +x youfinder
 cp youfinder /usr/bin
 echo "\n***done***\n"
 clear
-youfinder
-
+echo ""
 echo Setup Complete GO AHEAD!!!!!!        
+echo ""
+echo "----->Now you can use ruby youscanner from everywhere<-----"
